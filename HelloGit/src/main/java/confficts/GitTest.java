8c12935cdfcd4c82d6a807dc6858e5e467de645f
HuJunhao <1148240906@qq.com>
2022-01-06 14:35:19 +0800

    创建

---
+++ HelloGit/src/main/java/confficts/GitTest.java
@@ -0,0 +1,14 @@
+package confficts;
+
+/**
+ * @author junhao
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/1/614:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("HelloGit");
+    }
+}

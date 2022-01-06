5ea7bbfb6f4f3cca332eea20cba0a331238540c9
HuJunha <97151772+HuJunha@users.noreply.github.com>
2022-01-06 16:28:36 +0800

    Update HelloGit.java

--- HelloGit/src/main/java/HuJunhaoTest/HelloGit.java
+++ HelloGit/src/main/java/HuJunhaoTest/HelloGit.java
@@ -14,5 +14,7 @@ public class HelloGit {
         System.out.println("创建分支");
         System.out.println("分支添加");
         System.out.println("在线更新");
+        System.out.println("在线更新2");
+        System.out.println("在线更1");
     }
 }

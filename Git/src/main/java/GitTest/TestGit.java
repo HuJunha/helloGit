a1539a6acb4a12412d081ffaaf02ea0eaa3d113e
HuJunhao <1148240906@qq.com>
2022-01-06 15:41:36 +0800

    创建模块

---
+++ Git/src/main/java/GitTest/TestGit.java
@@ -0,0 +1,14 @@
+package GitTest;
+
+/**
+ * @author junhao
+ * @Title:
+ * @Package
+ * @Description:
+ * @date 2022/1/615:37
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("创建模块");
+    }
+}
